bbf36b289b483d398f8f6fbfbb29e5f6af81194c
Moksleivis <Moksleivis@kitm.local>
2023-03-31 12:20:45 +0300

    laba diena

---
+++ script.js
@@ -0,0 +1,81 @@
+// let vardas = "Gytis";
+// let amzius = 25;
+// let grupe = "JS1s";
+
+
+// console.log(`Mano vardas yra ${vardas}, mano amzius yra ${amzius} ir grupe yra ${grupe}`);
+// console.log("Mano vardas yra ${vardas}, mano amzius yra ${amzius} ir grupe yra ${grupe}");
+// console.log('Mano vardas yra ${vardas}, mano amzius yra ${amzius} ir grupe yra ${grupe}');
+// console.log(`Mano vardas yra ${vardas}, mano amzius yra 25 ir grupe yra ${grupe}`);
+
+
+// let vidurkis = 8.1;
+// let lankomumas = true;
+
+// if (vidurkis >= 8 && lankomumas) {
+//     console.log(`Jusu vidurkis yra ${vidurkis} ir lankomumas yra geras. todel stipendija gausite`);
+// } else {
+//     console.log("Stipendija nepriklauso");
+// }
+
+
+
+// let pajamos = 1200;
+// let vaikai = 2;
+
+// if (pajamos >= 800 && vaikai === 0) {
+//     console.log("Sveikinu paskola gausite");
+// } else if (pajamos >= 1000 && vaikai === 1) {
+//     console.log("Sveikinu paskola gausite ir su vaiku");
+// } else if (pajamos >= 1200 && vaikai === 2) {
+//     console.log("Sveikinu gavote paskola taip pat");
+// } else {
+//     console.log("Deja, paskolos negausite");
+// }
+
+
+
+// let pajamos = 1400;
+// let vaikai = 2;
+
+// if (pajamos >= 800 && vaikai === 0) {
+//     console.log(`Sveikinu paskola gausite`);
+// } else if (pajamos >= 1000 && vaikai === 1) {
+//     console.log("Sveikinu paskola gausite ir su vaiku");
+// } else if (pajamos >= 1200 && vaikai === 2) {
+//     console.log("Sveikinu gavote paskola taip pat ");
+// } else {
+//     console.log("Deja, paskolos negausite");
+// }
+
+
+let menesis = "Birzelis";
+
+switch(menesis) {
+    case "Gruodis":
+    case "Sausis":
+    case "Vasaris":
+        console.log(`Tu ivedei ${menesis}, o tai yra ziemos menesis`);
+        break;
+    case "Kovas":
+    case "Balandis":
+    case "Geguze":
+        console.log(`Tu ivedei ${menesis}, o tai yra pavasario menesis`);
+        break;
+    case "Birzelis":
+    case "Liepa":
+    case "Rugpjutis":
+        console.log(`Tu ivedei ${menesis}, o tai yra vasaros menesis`);
+        break;
+    case "Rugsejis":
+    case "Spalis":
+    case "Lapkritis":
+        console.log(`Tu ivedei ${menesis}, o tai yra rudens menesis`);
+        break;
+        default:
+            console.log("Netaisyklingai ivestas menesis");
+}
+
+// tht4rh4r5th45h45hb54hb4h54h5u65u56
+
+// rgjriugji3gjvieji93rgvi9e3gr
